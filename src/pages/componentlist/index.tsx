0dbb38c3d4785a8f69a51dd1e17c558cc88cdc2c
jarangit <jaran.dch@gmail.com>
2023-04-05 18:55:13 +0700

    feat:create all page

--- src/pages/componentlist/index.tsx
+++ src/pages/componentlist/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 type Props = {}
 
-const OrderPage = (props: Props) => {
+const ComponentListPage = (props: Props) => {
   return (
     <div className='min-h-screen w-full flex justify-center items-center'>
-      <div>This page is developing</div>
-    </div>)
+    <div>This page is developing</div>
+  </div>  )
 }
 
-export default OrderPage
+export default ComponentListPage
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 type Props = {}
 
-const OrderPage = (props: Props) => {
+const ProfilePage = (props: Props) => {
   return (
     <div className='min-h-screen w-full flex justify-center items-center'>
       <div>This page is developing</div>
-    </div>)
+    </div>
+  )
 }
 
-export default OrderPage
+export default ProfilePage
